bdd74e7eecbbfc215866342526b039a7ca1883f5
Dany Robert <rtdany10@gmail.com>
2025-11-18 11:39:05 +0530

    fix: don't sync item to shopify it its existing

--- ecommerce_integrations/shopify/product.py
+++ ecommerce_integrations/shopify/product.py
@@ -342,6 +342,9 @@ def upload_erpnext_item(doc, method=None):
 	if item.flags.from_integration:
 		return
 
+	if item.get("custom_existing_shopify_id"):
+		return
+
 	setting = frappe.get_doc(SETTING_DOCTYPE)
 
 	if not setting.is_enabled() or not setting.upload_erpnext_items:
