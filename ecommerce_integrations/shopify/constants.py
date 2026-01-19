ffd9c1054147d769e3e4cef14d07e2d9131a50bd
Dany Robert <rtdany10@gmail.com>
2026-01-19 12:56:02 +0530

    fix: bump shopify version

--- ecommerce_integrations/shopify/constants.py
+++ ecommerce_integrations/shopify/constants.py
@@ -6,7 +6,7 @@ MODULE_NAME = "shopify"
 SETTING_DOCTYPE = "Shopify Setting"
 OLD_SETTINGS_DOCTYPE = "Shopify Settings"
 
-API_VERSION = "2025-01"
+API_VERSION = "2026-01"
 
 WEBHOOK_EVENTS = [
 	"orders/create",
