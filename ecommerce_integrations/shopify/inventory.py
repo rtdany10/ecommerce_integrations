dc6a7e67ce8ea4dea400e8f3d40f34e52d2c6b96
Dany Robert <rtdany10@gmail.com>
2026-05-05 12:25:17 +0530

    fix: increase sleep to 1s

--- ecommerce_integrations/shopify/inventory.py
+++ ecommerce_integrations/shopify/inventory.py
@@ -120,7 +120,7 @@ def init_scheduled_shopify_update(inventory_sync_batch, warehous_map, default_lo
 			d.failure_reason = str(e)
 
 		frappe.db.commit()
-		time.sleep(0.6)
+		time.sleep(1)
 
 	_log_inventory_update_status(inventory_sync_batch)
 
